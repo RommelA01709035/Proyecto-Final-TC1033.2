33f0e6c8c3e0429a300b6d7da5379b395e0f1711
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2021-11-26 08:04:15 -0600

    Update and rename comida.h to .h

---
+++ .h
@@ -0,0 +1,19 @@
+#ifndef comida_H
+#define comida_H
+
+#include "Provedor.h"
+
+class Comida/*clase hija*/: public Provedor/*clase padre*/{
+	
+	private: //atributos
+		int piezas; 
+	
+	public: //metodos
+		Comida (std::string mar, std::string pro, int pie): Provedor(mar,pro,15)
+		{piezas = pie;}
+	
+		int getPiezas(){return piezas;}
+};
+
+
+#endif
