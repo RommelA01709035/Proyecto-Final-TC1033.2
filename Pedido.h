31f232d916263270fd60791040946579ad065797
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2021-12-02 20:14:42 -0600

    Update Pedido.h

--- Pedido.h
+++ Pedido.h
@@ -1,32 +1,35 @@
-#ifndef Pedido_H
-#define Pedido_H
-#include "Comida.h"
+#ifndef Pedido_H		//se utiliza mucho la clase asi que mejor
+#define Pedido_H		//la definimos
+
+#include "Comida.h"		//incluyo mis archivos
 #include "Provedor.h"
+//bibliotecas
 #include<iostream>
 #include<stdlib.h>
 #include<string.h>
  
 
-class Pedido/*clase*/{
-	private://atributos  
+class Pedido/*clase que se agrega*/{
+	private://declaro atributos  
 		int cantidad;
 		std::string id; 
-		Comida prov;
+		Comida prov; //agregación
 		 
 	
-	public: //metodos
-		Pedido(){}
+	public: //declaro metodos
+		Pedido(){}//constructor con llaves para listas
 		Pedido(std::string ID,int cant, Comida PROVEDOR){
 			cantidad = cant; 
 			id = ID;  
 			prov = PROVEDOR;  			 
-		}
+		}//constructor con la agregación
 		
-		//getters y setters agrupados
+		//getters agrupados
 		int getcantidad(){return cantidad; }
 		std::string getid(){return id; }
 		Comida getprov(){return prov;} 
 		
+		//setters agrupados
 		void setcantidad (int cant){cantidad = cant;}
 		void setprov (Comida PROVEDOR){prov = PROVEDOR;}
 		
